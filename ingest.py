945c22d5f9c06c976183068ecef0299d6a55c3b6
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2024-02-04 14:30:55 -0500

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -8,14 +8,11 @@ import chainlit as cl
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-custom_prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+custom_prompt_template = """
 
 Context: {context}
 Question: {question}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
 """
 
 def set_custom_prompt():
